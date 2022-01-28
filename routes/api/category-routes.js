c7166296bb389f868fbff7b10b09a2a220d12e75
Ross Westwater <ross.westwater@gmail.com>
2022-01-28 16:47:14 -0700

    installed current npms, updated models and their relationships. seeded db to test routes as they are written

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,11 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: [
+      
+    ],
+  })
 });
 
 router.get('/:id', (req, res) => {
